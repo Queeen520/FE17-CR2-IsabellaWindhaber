489f2b44e368b2c5b5a2b55d1a87d0cb9069137b
Isabella W <isabellawindhaber@gmail.com>
2022-09-30 19:31:17 +0200

    --

--- My Planner/script.js
+++ My Planner/script.js
@@ -1,3 +1,26 @@
 let myPlanner = JSON.parse(Planner);
 
 console.log(myPlanner);
+
+
+
+function addTask() {
+    return `
+    <img src="${myPlanner.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${myPlanner.taskName}</h5>
+      <p class="card-text">${myPlanner.description}</p>
+      <p class="card-text">${myPlanner.importance}</p>
+  </div>
+  `;
+}
+
+console.log(addTask());
+
+
+
+/*
+for (let task in myPlanner) {
+  document.getElementById("taskHere").innerHTML = task.addTask();
+}
+*/
