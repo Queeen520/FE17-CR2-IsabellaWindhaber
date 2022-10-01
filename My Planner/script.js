1a1d97430cd3450d6ed5efb221ebb225440ac98b
Isabella W <isabellawindhaber@gmail.com>
2022-10-01 03:51:20 +0200

    ..

--- My Planner/script.js
+++ My Planner/script.js
@@ -1,35 +1,64 @@
 let myPlanner = JSON.parse(Planner);
 
+console.log(typeof myPlanner);
 console.table(myPlanner);
 
-for (let task of myPlanner) {
+var mytask = document.getElementsByClassName("mydiv")[0];
 
-  document.getElementById("taskHere").innerHTML += `
-  
+
+for (task in myPlanner) {
+  mytask.innerHTML = `
   <div class="mydivclass">
-  <p>${myPlanner.taskName}</p>
-  <div><img src="MyPlanner/IMG/${myPlanner.image} width="100%"</div
-  <p>${myPlanner.description}</p>
-  <p>${myPlanner.importance}</p>
+  <img src="MyPlanner/IMG/${myPlanner[0].image} class="card-img-top" width="100%" alt="task">
+  <div class="card-body">
+    <h5 class="card-title">${myPlanner[0].taskName}</h5>
+    <p class="card-text">${myPlanner[0].description}</p>
+  </div>
+  <div class="card-footer">
+    <small class="text-muted"><p>${myPlanner[0].importance}</p></small>
   </div>
   `;
 }
 
+
+
+
+
+
+
+
+
+/* 
+function addTask() {
+mytask.innerHTML += `
+<div class="mydivclass">
+<div><img src="MyPlanner/IMG/${myPlanner.image} class="card-img-top" width="100%" alt="task"></div
+<div class="card-body">
+<h5>${myPlanner.taskName}</h5>
+<p>${myPlanner.description}</p>
+</div>
+<div class="card-footer mycardfooter">
+<small class="text-muted">${myPlanner.importance}</small>
+</div>
+`;
+}
+*/
 
 /* 
 
 function addTask() {
-    return `
-    <img src="${myPlanner.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${myPlanner.taskName}</h5>
-      <p class="card-text">${myPlanner.description}</p>
-      <p class="card-text">${myPlanner.importance}</p>
+
+  document.getElementById("div1").innerHTML += `
+  <div class="mydivclass">
+  <img src="/MyPlanner/IMG/${myPlanner.image} class="card-img-top" width="100%" alt="task">
+  <div class="card-body">
+    <h5 class="card-title">${myPlanner.taskName}</h5>
+    <p class="card-text">${myPlanner.description}</p>
+  </div>
+  <div class="card-footer">
+    <small class="text-muted"><p>${myPlanner.importance}</p></small>
   </div>
   `;
 }
 
-console.log(addTask());
-
 */
-
