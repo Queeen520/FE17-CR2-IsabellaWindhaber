9652fea612d8801ec7e8a8fc6eb06bfcb24fea83
Isabella W <isabellawindhaber@gmail.com>
2022-10-01 09:04:08 +0200

    -

--- My Planner/script.js
+++ My Planner/script.js
@@ -3,22 +3,34 @@ let myPlanner = JSON.parse(Planner);
 console.log(typeof myPlanner);
 console.table(myPlanner);
 
-var mytask = document.getElementsByClassName("mydiv")[0];
+var mytask = document.getElementById("taskhere");
 
 
-for (task in myPlanner) {
-  mytask.innerHTML = `
-  <div class="mydivclass">
-  <img src="MyPlanner/IMG/${myPlanner[0].image} class="card-img-top" width="100%" alt="task">
-  <div class="card-body">
-    <h5 class="card-title">${myPlanner[0].taskName}</h5>
-    <p class="card-text">${myPlanner[0].description}</p>
-  </div>
+for (task of myPlanner) {
+  console.log(task);
+  mytask.innerHTML += `
+  <div class="col mydivclass">
+    <img src="/FE17-CR2-IsabellaWindhaber/My Planner/${myPlanner.image} class="card-img-top" width="100%" alt="task">
+    <div class="card-body">
+    <h5 class="card-title">${myPlanner.taskName}</h5>
+    <p class="card-text">${myPlanner.description}</p>
+    </div>
   <div class="card-footer">
-    <small class="text-muted"><p>${myPlanner[0].importance}</p></small>
+    <small class="text-muted">Priority Level: <button type="button" class="btn-importance btn btn-dark">${myPlanner.importance}</button></small>
+  </div>
+  <div>
+    <button type="button" class="btn btn-success left-btn">green DONE</button>
+    <button type="button" class="btn btn-danger right-btn">red DELETE</button>
+  </div>
   </div>
   `;
 }
+
+document.getElementsByClassName("btn")[0].addEventListener = function () {
+  myPlanner[0].importance++;
+  document.getElementsByClassName("btn-importance")[0].innerHTML = myPlanner[0].importance;
+}
+
 
 
 
